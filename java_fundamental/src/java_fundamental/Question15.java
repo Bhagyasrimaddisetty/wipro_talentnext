68fb790f3f01d42ed9cdb9b7e8b0de39e39aeef9
Maddisetty Bhagyasri <maddisettybhagyasri@email.com>
2025-07-05 15:53:00 +0530

    Question15.java

--- java_fundamental/src/java_fundamental/Question15.java
+++ java_fundamental/src/java_fundamental/Question15.java
@@ -1,10 +1,29 @@
 package java_fundamental;
-
+import java.util.Scanner;
 public class Question15 {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int number = scanner.nextInt();
+boolean isPrime = true;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+if (number <= 1) {
+    isPrime = false;
+} else {
+    for (int i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+}
 
-	}
+if (isPrime) {
+    System.out.println(number + " is a prime number.");
+} else {
+    System.out.println(number + " is not a prime number.");
+        }
 
+        scanner.close();
+    }
 }
