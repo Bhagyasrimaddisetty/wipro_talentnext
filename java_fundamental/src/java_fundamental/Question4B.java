1383cd0f3de982a886beb6268ce3c4b40d2d62e2
Maddisetty Bhagyasri <maddisettybhagyasri@email.com>
2025-07-05 11:33:03 +0530

    Question4B.java

---
+++ java_fundamental/src/java_fundamental/Question4B.java
@@ -0,0 +1,22 @@
+package java_fundamental;
+import java.util.Scanner;
+public class Question4B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+int num1 =  sc.nextInt();
+int  num2 = sc.nextInt();
+if(num1>0 & num2>0) {
+	if(num1%10==num2%10) {
+System.out.println("True");
+	}else {
+		System.out.println("False");
+	}
+}
+	}
+}
+	
+	
+
+
