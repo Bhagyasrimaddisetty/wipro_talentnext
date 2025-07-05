842b807ecfada4e06d6a7ab1b323ec209877f037
Maddisetty Bhagyasri <maddisettybhagyasri@email.com>
2025-07-05 10:01:21 +0530

    Question1.java

---
+++ java_fundamental/src/java_fundamental/Question4.java
@@ -0,0 +1,11 @@
+package java_fundamental;
+import java.util.Scanner;
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+
+	}
+
+}
